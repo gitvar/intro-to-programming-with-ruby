37055f6d8af5c80bd93f6c6a09023c44caa7c92d
gitvar <benvanderm@gmail.com>
2016-02-12 15:35:05 +0200

    Updated both files to show 2 solutions. One with 'puts' and one with '.each'.

--- the-basics/exercise_3.rb
+++ the-basics/exercise_3.rb
@@ -1,5 +1,14 @@
 # exercise_2.rb
 
-movies = {Jaws: "1975", The_Incredibles: "2004", Gravity: "2013", Shrek: "2001", Raiders_of_the_Lost_Ark: "1981"}
+movies = {jaws: "1975", the_incredibles: "2004", gravity: "2013", shrek: "2001", raiders_of_the_lost_ark: "1981"}
 
-movies.each { |key, value| puts movies[key]}
+puts movies[:jaws]
+puts movies[:the_incredibles]
+puts movies[:gravity]
+puts movies[:shrek]
+puts movies[:raiders_of_the_lost_ark]
+
+# OR
+
+puts
+movies.each { |key, value| puts movies[key] }
--- the-basics/exercise_4.rb
+++ the-basics/exercise_4.rb
@@ -2,4 +2,12 @@
 
 movie_dates = ["1975", "2004", "2013", "2001", "1981"]
 
+puts movie_dates[0]
+puts movie_dates[1]
+puts movie_dates[2]
+puts movie_dates[3]
+puts movie_dates[4]
+
+# OR
+puts
 movie_dates.each { |date| puts date }
