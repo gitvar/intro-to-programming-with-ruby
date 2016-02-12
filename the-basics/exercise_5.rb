c5d138677d371528f1b59663a50c9c5dc8d00d20
gitvar <benvanderm@gmail.com>
2016-02-12 15:49:01 +0200

    First commit of exercise_5.rb

---
+++ the-basics/exercise_5.rb
@@ -0,0 +1,17 @@
+# exercise_5.rb
+
+# Factorial for 5:
+n5 = 5 * 4 * 3 * 2 * 1
+puts "5! = #{n5}"
+
+# Factorial of 6:
+n6 = 6 * n5
+puts "6! = #{n6}"
+
+# Factorial of 7:
+n7 = 7 * n6
+puts "7! = #{n7}"
+
+# Factorial of 8:
+n8 = 8 * n7
+puts "8! = #{n8}"
