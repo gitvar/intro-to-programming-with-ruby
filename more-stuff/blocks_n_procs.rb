ca3eaa9821d2fd5f8175c58a2e61463e11c75662
gitvar <benvanderm@gmail.com>
2016-02-19 12:33:11 +0200

    Added simple block example code from the book/course

--- more-stuff/blocks_n_procs.rb
+++ more-stuff/blocks_n_procs.rb
@@ -1,2 +1,15 @@
 # blocks_n_procks.rb
 
+def take_block(&block)
+  puts "Before executing the block ..."
+  puts
+  block.call
+  puts
+  puts "After block execution!"
+end
+
+take_block do
+  puts "This is the block"
+  puts "It is called from within the 'take_block' method!"
+  puts "Recognise the block usage from the '&' ampersand symbol"
+end
