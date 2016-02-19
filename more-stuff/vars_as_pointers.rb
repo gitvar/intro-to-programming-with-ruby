c0f923ddb2f2f4406ae72c8350f4b4c9e4cb9f37
gitvar <benvanderm@gmail.com>
2016-02-19 12:01:27 +0200

    Changed comment line lengths to fit into screen on browser

--- more-stuff/vars_as_pointers.rb
+++ more-stuff/vars_as_pointers.rb
@@ -10,13 +10,15 @@ a = "Goodbye!"
 puts "a = #{a}"
 puts "b = #{b}"
 puts
-# At the end, 'a' != 'b' as 'a' now 'points' to a new address in memory space (where the string 'Goodbye!' is stored). 'b' still 'points' to the original address in memory (where the string 'Hi there!' is stored).
+# At the end, 'a' != 'b' as 'a' now 'points' to a new address in memory space (where the string 'Goodbye!' is stored). 
+# 'b' still 'points' to the original address in memory (where the string 'Hi there!' is stored).
 
 a = b
 puts "a = #{a}"
 puts "b = #{b}"
-puts "Append 'How are you Bob?' to a"
+puts "Append the string 'How are you Bob?' to a"
 a << " How are you Bob?"
 puts "a = #{a}"
 puts "b = #{b}"
-# At the end 'a' == 'b' as 'a' was added to, or the memory space it 'points' to was modified, but the address of that memory space was not changed! 'b' still 'points' to that address in memory as well.
+# At the end 'a' == 'b' as 'a' was added to, or the memory space it 'points' to was modified, but the address of that 
+# memory space was not changed! 'b' still 'points' to that address in memory as well.
